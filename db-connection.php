b834d0e348514a4fff6f711f72c739812dc2703e
Bob McHenry <bobmchenryvideo@gmail.com>
2019-01-26 19:34:59 -0800

    Adding db-connection example file

---
+++ db-connection.php
@@ -0,0 +1,33 @@
+<?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "root";
+  $dbname = "sakila";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+  //Make a SELECT query
+  $query = "SELECT * FROM actor";
+
+  $result = $conn->query($query);
+
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          echo
+            "actor_id: "
+            . $row["actor_id"]
+            . " - Name: "
+            . $row["first_name"]
+            . " "
+            . $row["last_name"]
+            . "<br>";
+      }
+  }
+?>
