e059639f6dc097e18437c39e15e805987dc531a3
Bob McHenry <bobmchenryvideo@gmail.com>
2019-01-27 20:04:11 -0800

    added insert statement

--- db-connection.php
+++ db-connection.php
@@ -1,18 +1,28 @@
 <?php
   require('credentials.php');
-
   // Create connection
   $conn = new mysqli($servername, $username, $password, $dbname);
-
   // Check connection
   if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
   }
 
   //Make a SELECT query
-  $query = "SELECT * FROM actor";
+  $searchFor = "'McHenry'";
+  $selectQuery = "SELECT * FROM actor
+            WHERE last_name LIKE " . $searchFor;
 
-  $result = $conn->query($query);
+  //INSERT INTO table_name (column1, column2, column3, ...)
+  //VALUES (value1, value2, value3, ...);
+  // $insertQuery = "INSERT INTO actor (first_name, last_name)
+  //   VALUES ('Bob', 'McHenry')";
+  //
+  // if ($conn->query($insertQuery) === TRUE) {
+  //   echo "New stuff created successfully";
+  // } else {
+  //   echo "Error: " . $insertQuery . "<br>" . $conn->error;
+  // }
+  $result = $conn->query($selectQuery);
 
   if ($result->num_rows > 0) {
       // output data of each row
